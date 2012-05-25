d469568bc4c2b1827289affd6386faace64f931a
Rui Chicória <rchicoria@gmail.com>
2012-05-25 13:51:30 +0100

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ IntimeServer::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
