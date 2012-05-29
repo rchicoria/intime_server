5035de8b745078c8b19f1ffa1b2727faed73bbdd
Ricardo Lopes <mail@ricardolopes.net>
2012-05-29 04:21:20 +0100

    Changed migration to include StopPosition ID

--- db/migrate/20120509180132_create_stop_positions.rb
+++ db/migrate/20120509180132_create_stop_positions.rb
@@ -1,6 +1,6 @@
 class CreateStopPositions < ActiveRecord::Migration
   def change
-    create_table :stop_positions, :id => false do |t|
+    create_table :stop_positions do |t|
       t.integer :bus_id
       t.integer :bus_stop_id
       t.integer :position
